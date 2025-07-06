4ccc85a05f1d747b0387550328e2045b7d2e5966
v2clash <155278998+v2clash@users.noreply.github.com>
2025-07-07 07:32:56 +0800

    Update main.sh

--- .github/main.sh
+++ .github/main.sh
@@ -24,6 +24,7 @@ rm -rf $1
 
 git clone https://github.com/ophub/amlogic-s9xxx-armbian && mvdir amlogic-s9xxx-armbian
 wget -O ./action.yml https://raw.githubusercontent.com/ophub/amlogic-s9xxx-armbian/main/action.yml
+sed -i 's|kernel_repo="https://github.com/ophub/kernel"|kernel_repo="https://github.com/v2clash/build-armbian"|g' ./rebuild
 sed -i 's|default: "ophub/kernel"|default: "v2clash/build-armbian"|g' ./action.yml
 sed -i 's|default: "6.1.y_6.12.y"|default: "6.6.y_6.12.y"|g' ./action.yml
 sed -i 's/default: "-ophub"/default: ""/g' ./action.yml
